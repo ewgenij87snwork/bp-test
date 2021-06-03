4c243274c621dd69b327a96d5ebe24b5eb08f014
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-06-03 17:09:43 +0300

    Unit Test: @Input & HTML-Element

--- src/app/components/first/first.component.spec.ts
+++ src/app/components/first/first.component.spec.ts
@@ -1,10 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FirstComponent } from './first.component';
+import {Nums} from "../../service/types";
 
 describe('FirstComponent', () => {
   let component: FirstComponent;
   let fixture: ComponentFixture<FirstComponent>;
+
+  const mockNums: Nums = {
+    firstNum: 1,
+    secondNum: 2
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +22,21 @@ describe('FirstComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FirstComponent);
     component = fixture.componentInstance;
+    component.nums = mockNums;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display Nums', () => {
+    component.nums = {
+      firstNum: 2,
+      secondNum: 1
+    };
+    fixture.detectChanges();
+    const div = fixture.nativeElement.querySelectorAll('div.number')
+    expect(div[0].innerText).toEqual('2')
+  })
 });
