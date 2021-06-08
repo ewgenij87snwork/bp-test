15b574014a0c3e5baf99a9dfe96a958a765f19ec
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-06-08 15:38:20 +0300

    NgRX: Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {StoreService} from './service/store.service';
 import {ConnectorModule} from './components/connector.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -11,7 +15,11 @@ import {ConnectorModule} from './components/connector.module';
     ],
     imports: [
         BrowserModule,
-        ConnectorModule],
+        ConnectorModule,
+        StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
     providers: [StoreService],
     bootstrap: [AppComponent]
 })
