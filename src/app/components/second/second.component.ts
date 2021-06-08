c896c0937c4b3d9c31041f165582d394ddf806b6
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-06-08 16:22:15 +0300

    NgRX: Init Actions

--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,37 +1,37 @@
-import {Component, OnInit} from '@angular/core';
-import {StoreService} from '../../service/store.service';
+import {Component} from '@angular/core';
 import {Nums} from '../../service/types';
 import {interval} from 'rxjs';
-import {switchMap, takeWhile} from 'rxjs/operators';
+import {map, takeWhile} from 'rxjs/operators';
+import {Store} from '@ngrx/store';
+import {decrease, increase} from '../../reducers/nums';
 
 @Component({
     selector: 'app-second',
     templateUrl: './second.component.html',
     styleUrls: ['./second.component.scss']
 })
-export class SecondComponent implements OnInit {
+export class SecondComponent {
     nums: Nums = null;
     stop = false;
 
-    constructor(private storeService: StoreService) {
+    constructor(private store: Store) {
     }
 
     interval = interval(1000);
-
-    ngOnInit(): void {
-        this.nums = this.storeService.initNums();
-    }
 
     onStart(): void {
         this.stop = false;
         this.interval
             .pipe(
                 takeWhile(() => !this.stop),
-                switchMap(async () => this.storeService.change()))
-            .subscribe(nums => this.nums = nums);
+                map(async () => {
+                    this.store.dispatch(increase());
+                    this.store.dispatch(decrease());
+                    this.store.dispatch(decrease());
+                }))
+            .subscribe();
     }
 
     onReset(): void {
-        this.nums = this.storeService.initNums();
     }
 }
---
+++ src/app/reducers/nums.ts
@@ -0,0 +1,5 @@
+import {createAction} from '@ngrx/store';
+
+export const change = createAction('[NUMS] change');
+export const increase = createAction('[NUMS] increase');
+export const decrease = createAction('[NUMS] decrease');
